698d8a9d5685a851ec055585c40f5d09124aed7a
Szzz666 <3214948198@qq.com>
2025-01-05 13:52:30 +0800

    修复命令错误问题

--- src/main/java/top/szzz666/RealTime/command/ReloadCommand.java
+++ src/main/java/top/szzz666/RealTime/command/ReloadCommand.java
@@ -3,18 +3,21 @@ package top.szzz666.RealTime.command;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 
+import static top.szzz666.RealTime.config.RtConfig.ReloadCmd;
 import static top.szzz666.RealTime.config.RtConfig.loadConfig;
 
 
 public class ReloadCommand extends Command {
     public ReloadCommand() {
-        super("CommandName", "重新加载RealTime插件配置文件");
+        super(ReloadCmd, "重新加载RealTime插件配置文件");
     }
 
     @Override
     public boolean execute(CommandSender sender, String label, String[] args) {
         if (sender.isPlayer()) {
             loadConfig();
+            sender.sendMessage("RealTime配置文件重新加载成功！");
+            return true;
         }
         return false;
     }
